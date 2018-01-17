b3d7a21d81845f23483e7f6307453b3810a0a13c
Cipher <electimon@gmail.com>
2018-01-17 00:01:06 -0500

    Adding function -3
    Woottt

--- main.sh
+++ main.sh
@@ -18,7 +18,7 @@ if [ $varq = y ]
 then
 echo Linux
 fi
-delcare -a os=linux
+declare -a os=linux
 fi
 if [ $varq = y ] 
 then
@@ -30,24 +30,49 @@ echo "Enter model in idenifier form."
 read -a model
 echo "Enter ECID in any form."
 read -a ecid
-echo 'Enter versions, like this "8.0.2 "7.0.2" "7.0.3"'
+echo 'Enter version'
 read -a VERSIONS
 echo ok
 
-for VERSION in ${VERSIONS[@]}
-do
+#for VERSION in ${VERSIONS[@]}
+#do
 	if [ $blobby = "y" ]
 	then
-	`find ./ -name tsschecker_$os` -s -i $VERSION -d $model -e $ecid 
-done
+	`find ./ -name tsschecker_$os` -s -i $VERSIONS -d $model -e $ecid 
+	fi
+	`find ./ -name tsschecker_$os` -i $VERSIONS -d $model -e $ecid
+#done
 else
-echo "Not found, sad, getting now"
+if [ `find ./ -name tsschecker*$os` = tsschecker*$os ]] || if [ `find ./ -name tsschecker` = tsschecker ]
+then
+echo "Enter model in idenifier form."
+read -a model
+echo "Enter ECID in any form."
+read -a ecid
+echo 'Enter versions, like this "8.0.2 "7.0.2" "7.0.3"'
+read -a VERSIONS
+echo ok
+chmod +x tsschecker
+
+#for VERSION in ${VERSIONS[@]}
+#do
+        if [ $blobby = "y" ]
+        then
+        ./tsschecker -s -i $VERSIONS -d $model -e $ecid
+        fi
+        ./tsschecker -i $VERSIONS -d $model -e $ecid
+fi
+fi
+echo "Tsschecker not found, sad, getting now"
 if [ $varq = y ]
 then
 echo "Not found, :("
 sleep 1
 echo "DLING, :)"
-curl "https://github.com/electimon/TatsuBash/blob/master/tssfromrepo/tsschecker_linux?raw=true" > tsschecker
+fi
+curl "https://raw.githubusercontent.com/electimon/TatsuBash/master/tssfromrepo/tsschecker_$os" > tsschecker
+if [ $varq = "y" ]
+then
 echo "Done"
 fi
 echo "Enter model in idenifier form."
@@ -57,16 +82,14 @@ read -a ecid
 echo 'Enter versions, like this "8.0.2 "7.0.2" "7.0.3"'
 read -a VERSIONS
 echo ok
+chmod +x tsschecker
 
-for VERSION in ${VERSIONS[@]}
-do
+#for VERSION in ${VERSIONS[@]}
+#do
         if [ $blobby = "y" ]
         then
-        ./tsschecker -s -i $VERSION -d $model -e $ecid
-done
-
-
-
-
-fi
+        ./tsschecker -s -i $VERSIONS -d $model -e $ecid
+	fi
+	./tsschecker -i $VERSIONS -d $model -e $ecid
+#done
 
