ee7a1dcb2bd9e20266252c4c4b155c55aad9121b
Cipher <electimon@gmail.com>
2018-01-16 23:15:26 -0500

    Adding function -2
    Woot!!

--- main.sh
+++ main.sh
@@ -1,13 +1,54 @@
 #!/bin/bash
+echo "Debug? y,n"
+read varq
+echo "Save blobs? y,n"
+read blobby
 echo "Detecting os"
 if [[ $(uname) = "Darwin" ]]
 then
+if [ $varq = y ]
+then
 echo MacOS
-declare -a 
+fi
+declare -a os=macos
 fi
 if [[ $(uname -s) = "Linux" ]]
 then
+if [ $varq = y ] 
+then
 echo Linux
+fi
+delcare -a os=linux
+fi
+if [ $varq = y ] 
+then
+echo "Searching for tsschecker in directory..."
+fi
+if [[ $(find ./ -name tsschecker_$os) = "tsschecker_$os" ]]
+then
+echo "Enter model in idenifier form."
+read -a model
+echo "Enter ECID in any form."
+read -a ecid
+echo 'Enter versions, like this "8.0.2 "7.0.2" "7.0.3"'
+read -a VERSIONS
+echo ok
+
+for VERSION in ${VERSIONS[@]}
+do
+	if [ $blobby = "y" ]
+	then
+	`find ./ -name tsschecker_$os` -s -i $VERSION -d $model -e $ecid 
+done
+else
+echo "Not found, sad, getting now"
+if [ $varq = y ]
+then
+echo "Not found, :("
+sleep 1
+echo "DLING, :)"
+curl "https://github.com/electimon/TatsuBash/blob/master/tssfromrepo/tsschecker_linux?raw=true" > tsschecker
+echo "Done"
 fi
 echo "Enter model in idenifier form."
 read -a model
@@ -19,5 +60,13 @@ echo ok
 
 for VERSION in ${VERSIONS[@]}
 do
-	echo hi
+        if [ $blobby = "y" ]
+        then
+        ./tsschecker -s -i $VERSION -d $model -e $ecid
 done
+
+
+
+
+fi
+
