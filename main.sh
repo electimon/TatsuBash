56f9cf033ac983adcbbd9d2c0231a6d5b4f7420e
Cipher <electimon@gmail.com>
2018-01-16 23:06:24 -0500

    Added function
    Woot :+1:

--- main.sh
+++ main.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+echo "Detecting os"
+if [[ $(uname) = "Darwin" ]]
+then
+echo MacOS
+declare -a 
+fi
+if [[ $(uname -s) = "Linux" ]]
+then
+echo Linux
+fi
 echo "Enter model in idenifier form."
-read MODEL
+read -a model
 echo "Enter ECID in any form."
-read ECID
-echo "Enter versions, like this "8.0.2 "7.0.2" "7.0.3""
-read VERSIONS
+read -a ecid
+echo 'Enter versions, like this "8.0.2 "7.0.2" "7.0.3"'
+read -a VERSIONS
 echo ok
-for VERSION in "${VERSIONS[@]}"
+
+for VERSION in ${VERSIONS[@]}
 do
-	./tsschecker_macos -d $MODEL -e $ECID -s -i $VERSION
+	echo hi
 done
